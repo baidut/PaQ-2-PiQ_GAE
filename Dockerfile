a8623ede457ec29771ef79d10cc29bc5045edc3b
Zhenqiang Ying <yingzhenqiang@gmail.com>
2020-02-06 22:05:55 -0600

    + global score prediction is working now

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM python:3.6-slim-stretch
 RUN apt update
 RUN apt install -y python3-dev gcc
 
+RUN pip install torch==1.4.0+cpu torchvision==0.5.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
 ADD requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
--- app/server.py
+++ app/server.py
@@ -5,12 +5,15 @@ from starlette.middleware.cors import CORSMiddleware
 import uvicorn, aiohttp, asyncio
 from io import BytesIO
 
+"""
 from fastai import *
 from fastai.vision import *
+"""
+from fastiqa.all import *
 
-model_file_url = 'https://www.dropbox.com/s/y4kl2gv1akv7y4i/stage-2.pth?raw=1'
+model_file_url = 'https://github.com/baidut/PaQ-2-PiQ/releases/download/v1.0/RoIPoolModel-fit.10.bs.120.pth'
 model_file_name = 'model'
-classes = ['black', 'grizzly', 'teddys']
+# classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
 app = Starlette()
@@ -26,9 +29,16 @@ async def download_file(url, dest):
 
 async def setup_learner():
     await download_file(model_file_url, path/'models'/f'{model_file_name}.pth')
+    """
     data_bunch = ImageDataBunch.single_from_classes(path, classes,
         ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
     learn = cnn_learner(data_bunch, models.resnet34, pretrained=False)
+    learn.load(model_file_name)
+    """
+    data = Im2MOS(TestImages(path=path))
+    data.device = torch.device('cpu') # run on CPU
+    model = RoIPoolModel()
+    learn = RoIPoolLearner(data, model, path=path) 
     learn.load(model_file_name)
     return learn
 
@@ -47,7 +57,11 @@ async def analyze(request):
     data = await request.form()
     img_bytes = await (data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    return JSONResponse({'result': str(learn.predict(img)[0])})
+
+    qmap = learn.predict_quality_map(img, [32, 32])
+    score = f'{qmap.global_score:.2f}'
+    mat = qmap.mat.astype(int).tolist()
+    return JSONResponse({'result': score})
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=8080)
